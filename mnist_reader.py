ec84518556e183a0d7b0c658452b1124d790d88a
Neofly4023 <ea_remache@esi.dz>
2018-08-20 05:58:28 +0100

    First working version

---
+++ mnist_reader.py
@@ -0,0 +1,42 @@
+import numpy as np
+
+def load_mnist(path, kind='train'):
+    import os
+    import gzip
+    import numpy as np
+
+    """Load MNIST data from `path`"""
+    labels_path = os.path.join(path,
+                               '%s-labels-idx1-ubyte.gz'
+                               % kind)
+    images_path = os.path.join(path,
+                               '%s-images-idx3-ubyte.gz'
+                               % kind)
+
+    with gzip.open(labels_path, 'rb') as lbpath:
+        labels = np.frombuffer(lbpath.read(), dtype=np.uint8,
+                               offset=8)
+
+    with gzip.open(images_path, 'rb') as imgpath:
+        images = np.frombuffer(imgpath.read(), dtype=np.uint8,
+                               offset=16).reshape(len(labels), 784)
+    return images, labels
+
+
+def valid_imshow_data(data):
+    data = np.asarray(data)
+    if data.ndim == 2:
+        return True
+    elif data.ndim == 3:
+        if 3 <= data.shape[2] <= 4:
+            return True
+        else:
+            print('The "data" has 3 dimensions but the last dimension '
+                  'must have a length of 3 (RGB) or 4 (RGBA), not "{}".'
+                  ''.format(data.shape[2]))
+            return False
+    else:
+        print('To visualize an image the data must be 2 dimensional or '
+              '3 dimensional, not "{}".'
+              ''.format(data.ndim))
+        return False
